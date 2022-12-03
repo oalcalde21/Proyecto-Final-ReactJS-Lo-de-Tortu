a2ca00561b963257213ff7086b1ad91c28dd8093
oalcalde21 <oscar.alcalde@latam.rci.com>
2022-12-03 14:26:47 -0300

    Se sacan algunos comentarios

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCFnqjgT4BNJb8Mp2gp0oqxooskMLvktug",
   authDomain: "lo-de-tortu.firebaseapp.com",
