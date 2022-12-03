6b5310dfedbacb0f0df754cb21ac2363c476b59c
oalcalde21 <oscar.alcalde@latam.rci.com>
2022-12-03 13:41:01 -0300

    coreeciones de detalles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 export { default as Home } from './Home';
 export { default as Details } from './Details';
 export { default as Tienda } from './Tienda';
-export { default as NotFound } from './NotFound';
 export { default as Nosotros } from './Nosotros';
